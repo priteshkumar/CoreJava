8d66c3f82dd06f2f2821401ba92496d0714cab2a
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-07-30 11:29:52 +0530

    add FunctionalInteface demo

---
+++ src/main/java/com/mavixk/jcp/lambdaexp/pls/finterface/FunctionalIntfTest.java
@@ -0,0 +1,34 @@
+package com.mavixk.jcp.lambdaexp.pls.finterface;
+
+import java.util.function.Predicate;
+
+public class FunctionalIntfTest {
+
+  public static void main(String[] args) {
+    testPredicate();
+  }
+
+  public static void testPredicate() {
+    Predicate<String> stringPredicate = (s) -> s.length() > 10;
+    System.out.println(stringPredicate.test("awscloudplatform"));
+  }
+
+  /**
+   * Functional interface should have only one abstract method
+   * default/static methods dont count
+   * Object methods do not count
+   * @param <T>
+   */
+  @FunctionalInterface
+  interface ParamTest<T> {
+
+    public boolean testParam(T val);
+
+    //default methods dont count
+    default boolean testVal(T val){
+      return true;
+    }
+    //Object overridden methods do not count
+    boolean equals(Object object);
+  }
+}
