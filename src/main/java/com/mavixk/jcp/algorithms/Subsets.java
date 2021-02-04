10933dcfcfb9f527bdbbf2bef742c36a14033977
Pritesh Kumar <pritesh.kumar2@gmail.com>
2021-02-04 11:39:53 +0530

    add subset gen solution

---
+++ src/main/java/com/mavixk/jcp/algorithms/Subsets.java
@@ -0,0 +1,35 @@
+package com.mavixk.jcp.algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets {
+
+  public static List<List<Integer>> genSubsets(int[] a, List<List<Integer>> res, List<Integer> out,
+      int index) {
+    if(index < 0){
+     // System.out.println(out);
+      res.add(out);
+      return res;
+    }
+    List<Integer> temp = new ArrayList<>(out);
+    temp.add(a[index]);
+    List<List<Integer>> m = genSubsets(a,res,temp,index-1);
+    List<Integer> temp1 = new ArrayList<>(temp);
+    temp1.remove(temp1.size()-1);
+    return genSubsets(a,res,temp1,index -1);
+
+  }
+
+  public static List<List<Integer>> subsets(int[] a) {
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> out = new ArrayList<>();
+    return genSubsets(a, res, out, a.length - 1);
+  }
+
+  public static void main(String[] args) {
+    int[] a = {1, 2, 3,4};
+    System.out.println(subsets(a));
+  }
+
+}
